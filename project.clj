f2907779187a789ab4dda7ca4527e4513c0cb622
williamgrimes <williamgrimes@gmx.com>
2021-10-25 11:59:34 +0200

    Add seed for randomness and lein main

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject quil-workflow "1.0.0-SNAPSHOT"
+  :main quil-workflow.core
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [quil "2.2.1"]])
+                 [quil "2.2.1"]
+                 [random-seed "1.0.0"]])
--- src/quil_workflow/core.clj
+++ src/quil_workflow/core.clj
@@ -1,9 +1,16 @@
 (ns quil-workflow.core
   (:require [quil.core :as q])
-  (:require [quil-workflow.dynamic :as dynamic]))
+  (:require [quil-workflow.dynamic :as dynamic])
+  (:require [random-seed.core :refer :all])
+  (:refer-clojure :exclude [rand rand-int rand-nth shuffle])
+  )
+
+(set-random-seed! 7)
 
 (q/defsketch example
   :title "Oh so many grey circles"
   :setup dynamic/setup
   :draw dynamic/draw
   :size [1920 1080])
+
+(defn -main [& args])

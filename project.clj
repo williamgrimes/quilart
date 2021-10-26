f3f880d287465090da657640b1992a464a38385c
williamgrimes <williamgrimes@gmx.com>
2021-10-26 10:37:45 +0200

    Parse arguments from the command line

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject quil-workflow "1.0.0-SNAPSHOT"
   :main quil-workflow.core
   :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/tools.cli "1.0.206"]
                  [quil "2.2.1"]
                  [random-seed "1.0.0"]])
--- src/quil_workflow/core.clj
+++ src/quil_workflow/core.clj
@@ -1,13 +1,20 @@
 (ns quil-workflow.core
   (:require [quil.core :as q])
   (:require [quil-workflow.dynamic :as dynamic])
-  )
+  (:require [clojure.tools.cli :refer [parse-opts]]))
 
+(def cli-config [[nil "--help" "Print this help" :default false]
+                 ["-w" "--width WIDTH" "Image width" :default 400]
+                 ["-h" "--height HEIGHT" "Image height" :default 400]
+                 ["-s" "--seed SEED" "Random seed value" :default 42]
+                 ])
 
-(q/defsketch example
-  :title "Oh so many grey circles"
-  :setup dynamic/setup
-  :draw dynamic/draw
-  :size [1920 1080])
-
-(defn -main [& args])
+(defn -main [& args]
+  (let [{:keys [options arguments summary errors]}
+        (parse-opts args cli-config)]
+    (if (:help options)
+      (println summary)
+      (q/defsketch sketch
+       :setup dynamic/setup
+       :draw dynamic/draw
+       :size [(:width options) (:height options)]))))

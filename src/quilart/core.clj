fd3904212aebc1c4d7687ca011cbabe2a075b237
williamgrimes <williamgrimes@gmx.com>
2021-10-27 11:31:05 +0200

    Move palettes to color.clj

--- src/quilart/core.clj
+++ src/quilart/core.clj
@@ -1,5 +1,6 @@
 (ns quilart.core
   (:gen-class)
+  (:require [quilart.color :as c])
   (:require [quil.core :as q])
   (:require [clojure.tools.cli :refer [parse-opts]])
   (:require [random-seed.core :refer :all])
@@ -8,32 +9,12 @@
 ;TODO rename project
 ;TODO output file as CLI
 ;TODO use svg instead of png
-;TODO move color-schemes to palettes.clj
 ;FIXME fix background color seed
 
-(def nord-pallete
-  (hash-map
-   :nord0  [46  52  64 ]
-   :nord1  [59  66  82 ]
-   :nord2  [67  76  94 ]
-   :nord3  [76  86  106]
-   :nord4  [216 222 233]
-   :nord5  [229 233 240]
-   :nord6  [236 239 244]
-   :nord7  [143 188 187]
-   :nord8  [136 192 208]
-   :nord9  [129 161 193]
-   :nord10 [94  129 172]
-   :nord11 [191 97  106]
-   :nord12 [208 135 112]
-   :nord13 [235 203 139]
-   :nord14 [163 190 140]
-   :nord15 [180 142 173]
-   ))
 
 (defn draw-circle []
-    (let [color1 (rand-nth (vals nord-pallete))
-          color2 (rand-nth (vals nord-pallete))
+    (let [color1 (rand-nth (vals c/nord-palette))
+          color2 (rand-nth (vals c/nord-palette))
           diam   (rand-int 100)
           x      (rand-int (q/width))
           y      (rand-int (q/height))]
@@ -45,8 +26,8 @@
                     "\tcolor2: " color2 "\tdiam: " diam))))
 
 (defn draw-triangle []
-    (let [color1 (rand-nth (vals nord-pallete))
-          color2 (rand-nth (vals nord-pallete))
+    (let [color1 (rand-nth (vals c/nord-palette))
+          color2 (rand-nth (vals c/nord-palette))
           diam   (rand-int 100)
           x      (rand-int (q/width))
           y      (rand-int (q/height))]
